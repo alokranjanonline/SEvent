9abaa1b8aa4cede3fc857ececd35eb6081d2f977
aloksport <alokranjan999@gmail.com>
2023-12-12 17:49:58 +0530

    12-Dec-2023

---
+++ app/src/main/java/com/example/sevent/ViewPagerAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.sevent
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragmentManager: FragmentManager, private val lifecycle:Lifecycle) :
+    FragmentStateAdapter(fragmentManager.lifecycle) {
+
+    // declare arrayList to contain fragments and its title
+    private val mFragmentList = ArrayList<Fragment>()
+    private val mFragmentTitleList = ArrayList<String>()
+
+    override fun getItem(position: Int): Fragment {
+        // return a particular fragment page
+        return mFragmentList[position]
+    }
+
+    override fun getCount(): Int {
+        // return the number of tabs
+        return mFragmentList.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence{
+        // return title of the tab
+        return mFragmentTitleList[position]
+    }
+
+    fun addFragment(fragment: Fragment, title: String) {
+        // add each fragment and its title to the array list
+        mFragmentList.add(fragment)
+        mFragmentTitleList.add(title)
+    }
+}
